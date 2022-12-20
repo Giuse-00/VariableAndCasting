0ffb8a817a8b7649a2c427f2e616f2a6a2a9bcb8
Giuseppe Iacono <giuseiacono10@gmail.com>
2022-12-20 13:00:50 +0100

    Iacono esercizio Variables and casting between types

---
+++ src/main/kotlin/Start.kt
@@ -0,0 +1,16 @@
+fun main(){
+
+    val n1 : Int = 7
+    val n2 : Double = 11.6
+    val n3 : Short = 2
+
+    val sum = n1 + n2 + n3
+
+    println("The sum between n1, n2 and n3 is: $sum")
+
+    // or
+
+    println("The sum between n1, n2 and n3 is: ${n1 + n2 + n3}")
+
+
+}
